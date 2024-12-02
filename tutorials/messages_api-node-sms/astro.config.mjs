4855232c6448f1082e4cdacf511b30c36b4d503b
Abdulhakim Ajetunmobi <abdul.ajetunmobi@vonage.com>
2024-12-02 15:54:03 +0000

    Dynamic toolbar loading (#6)

--- tutorials/messages_api-node-sms/astro.config.mjs
+++ tutorials/messages_api-node-sms/astro.config.mjs
@@ -1,17 +1,20 @@
 import { defineConfig } from 'astro/config';
+
+import markdoc from '@astrojs/markdoc';
 import starlight from '@astrojs/starlight';
 import relativeLinks from 'astro-relative-links';
-import myIntegration from './my-toolbar-app/my-integration.ts';
+import vonageIntegration from './vonage-toolbar/integration.ts';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     relativeLinks(),
-    myIntegration,
+    vonageIntegration,
     starlight({
       title: 'Vonage Onboarding',
       tableOfContents: false,
       pagefind: false,
     }),
+    markdoc({ allowHTML: true })
   ],
 });
