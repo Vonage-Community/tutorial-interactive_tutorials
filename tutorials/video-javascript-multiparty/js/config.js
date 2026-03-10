5ffa177f9ce1af9f2d1bbe9b765aebe19b4b78d0
Dwane Hemmings <dwane.hemmings@vonage.com>
2026-03-10 14:09:58 -0400

    Delete tutorials/video-javascript-multiparty directory

--- tutorials/video-javascript-multiparty/js/config.js
+++
@@ -1,4 +0,0 @@
-// Set this to the base URL of your sample server, such as 'https://your-app-name.demoapplication.com'.
-// Do not include the trailing slash.
-
-const SAMPLE_SERVER_BASE_URL = 'https://YOUR-SERVER-URL';
